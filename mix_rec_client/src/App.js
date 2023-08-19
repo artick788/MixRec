74228ca98a354faffa6dd374bf39890acc77c0e3
artick788 <60552965+artick788@users.noreply.github.com>
2023-08-19 17:37:34 +0200

    fixed navigation bar

--- mix_rec_client/src/App.js
+++ mix_rec_client/src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Downloader from "./Pages/Downloader";
+import Overview from "./Pages/Overview";
+import Recommendations from "./Pages/Recommendations";
+import {AppBar, Toolbar, Tabs, Tab} from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <Tabs>
+            <Tab label="Downloader" href="/downloader" />
+            <Tab label="Overview" href="/overview" />
+            <Tab label="Recommendations" href="/recommendations" />
+          </Tabs>
+        </Toolbar>
+      </AppBar>
+      <Router>
+        <Routes>
+          <Route path="/downloader" element={<Downloader />} />
+          <Route path="/overview" element={<Overview />} />
+          <Route path="/recommendations" element={<Recommendations />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ mix_rec_client/src/Pages/Downloader.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Downloader() {
+  return (
+    <div>
+      <h1>Downloader</h1>
+    </div>
+  );
+}
