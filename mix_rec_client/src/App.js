c675cc0ce2ed8277322d9a92047f11a4fd4339bc
artick788 <60552965+artick788@users.noreply.github.com>
2023-08-19 18:05:39 +0200

    added download page

--- mix_rec_client/src/App.js
+++ mix_rec_client/src/App.js
@@ -6,27 +6,33 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Downloader from "./Pages/Downloader";
 import Overview from "./Pages/Overview";
 import Recommendations from "./Pages/Recommendations";
-import {AppBar, Toolbar, Tabs, Tab} from "@mui/material";
+import {AppBar, Toolbar, Tabs, Tab, ThemeProvider, createTheme} from "@mui/material";
+
+const theme = createTheme({
+
+});
 
 function App() {
   return (
     <div className="App">
-      <AppBar position="static">
-        <Toolbar>
-          <Tabs>
-            <Tab label="Downloader" href="/downloader" />
-            <Tab label="Overview" href="/overview" />
-            <Tab label="Recommendations" href="/recommendations" />
-          </Tabs>
-        </Toolbar>
-      </AppBar>
-      <Router>
-        <Routes>
-          <Route path="/downloader" element={<Downloader />} />
-          <Route path="/overview" element={<Overview />} />
-          <Route path="/recommendations" element={<Recommendations />} />
-        </Routes>
-      </Router>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar>
+            <Tabs>
+              <Tab label="Downloader" href="/downloader" />
+              <Tab label="Overview" href="/overview" />
+              <Tab label="Recommendations" href="/recommendations" />
+            </Tabs>
+          </Toolbar>
+        </AppBar>
+        <Router>
+          <Routes>
+            <Route path="/downloader" element={<Downloader />} />
+            <Route path="/overview" element={<Overview />} />
+            <Route path="/recommendations" element={<Recommendations />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }
--- mix_rec_client/src/Pages/Downloader.js
+++ mix_rec_client/src/Pages/Downloader.js
@@ -1,10 +1,102 @@
-import React from "react";
+import React, {useState} from "react";
+import {Box, TextField, Typography, Grid, Button} from "@mui/material";
 
 
 export default function Downloader() {
+  const [url, setUrl] = useState("");
+  const [artist, setArtist] = useState("");
+  const [title, setTitle] = useState("");
+  const [album, setAlbum] = useState("");
+  const [genre, setGenre] = useState("");
+
+  const download = () => {
+
+  }
+
   return (
     <div>
-      <h1>Downloader</h1>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh', // Ensures the div covers the full viewport height
+      }}>
+        <Box sx={{ width: '100%' }}>
+          <Grid container spacing={2}>
+
+            <Grid item xs={12}>
+              <Typography variant="h4" component="div" gutterBottom>
+                Download
+              </Typography>
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                id="url"
+                label="URL"
+                variant="outlined"
+                value={url}
+                sx={{ width: '30%' }}
+                onChange={(event) => setUrl(event.target.value)}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                id="artist"
+                label="Artist"
+                variant="outlined"
+                value={artist}
+                sx={{ width: '30%' }}
+                onChange={(event) => setArtist(event.target.value)}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                id="title"
+                label="Title"
+                variant="outlined"
+                value={title}
+                sx={{ width: '30%' }}
+                onChange={(event) => setTitle(event.target.value)}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                id="album"
+                label="Album"
+                variant="outlined"
+                value={album}
+                sx={{ width: '30%' }}
+                onChange={(event) => setAlbum(event.target.value)}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                id="genre"
+                label="Genre"
+                variant="outlined"
+                value={genre}
+                sx={{ width: '30%' }}
+                onChange={(event) => setGenre(event.target.value)}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                sx={{ width: '30%' }}
+                onClick={download}
+              >
+                Download
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </div>
     </div>
   );
 }
